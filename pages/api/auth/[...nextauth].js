aa80d76ad169d6fe364030339cb93a66091522d1
Hayden Cleaver <rexcleaver90@gmail.com>
2022-11-18 00:46:48 +0000

    init auth commit

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,15 @@
+import NextAuth from "next-auth/next";
+import GithubProvider from "next-auth/providers/github"
+import GoogleProvider from "next-auth/providers/google";
+export default NextAuth ({
+
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET
+    }),
+    
+  ],
+})
+
+// export default NextAuth(authOptions);
