ee1113c40bfff2c81876acf0125b48561efe0f7f
Isaiah Keller <isaiah.keller@gmail.com>
2022-11-18 23:45:37 +0000

    books api retrieving appropriate information

--- pages/api/books.js
+++ pages/api/books.js
@@ -1,34 +1,41 @@
 
 
-const books = async (req, res) => {
-  
-  console.log('req: ', req)
+const books = async () => {
 
   const axios = require('axios');
   const env = require('env');
 
   let BOOKS_API = process.env.NEXT_PUBLIC_BOOKS_API;
-  let BOOKS_Key = process.env.BOOKS_Key;
-  let query = req.query || 'lord of the rings';
+  let BOOKS_Covers = process.env.NEXT_PUBLIC_BOOKS_Covers
+  let query = 'wastelands stephen king';
 
   const queryFormatter = (string) => {
-    if(string){
-      string.split(' ');
-      string.join('+');
+    if (string.includes(' ')) {
+      let newArray = string.split(' ');
+      return newArray.join('+');
     }
   }
 
-  let fQuery = queryFormatter(query);
+  let fQuery = queryFormatter(query) || query;
 
   let url = `${BOOKS_API}${fQuery}`;
+  let res;
 
   try {
 
-    const res = await axios.get(url)
-      .then(response => {
-        console.log('response.data: ', response.data)
+    await axios.get(url)
+      .then(response1 => {
+        // console.log('books response.data.docs[0]: ', response1.data.docs[0])
+
+        res = {
+          title: response1.data.docs[0].title,
+          author: response1.data.docs[0].author_name[0],
+          image: `${BOOKS_Covers}id/${response1.data.docs[0].cover_i}-M.jpg`,
+          year: response1.data.docs[0].first_publish_year,
+        }
       })
-  } catch(error) {
+    console.log('books res: ', res)
+  } catch (error) {
     console.error('Error in books.js: ', error)
   }
 }
