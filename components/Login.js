6a955f12b1ba029d12c8e3e6fb148e495ceb3d4d
Junyoung Son <junyoungson808@gmail.com>
2022-11-21 21:39:05 +0000

    update

--- components/Login.js
+++ components/Login.js
@@ -1,24 +1,24 @@
 import Link from 'next/link'
-import navStyles from '../styles/Nav.module.css'
+import loginStyles from '../styles/Nav.module.css'
 import { signIn, signOut, useSession } from 'next-auth/react';
 
 const Login = () => {
 
-    const {data: session, status } = useSession();
+    const { data: session, status } = useSession();
     console.log(session)
-  
+
     const user = session?.user;
 
     return (
         <>
             <login >
-                <div>
+                <div sx={{display: 'flex'}}>
                     {user && (
-                        <>
-                            <img src={session.user.image} height='50' width='50' />
-                            <h2>Welcome, {user.name}</h2>
+                        <div sx={{display: 'flex'}}>
+                            <img alt='avatar' src={session.user.image} height='50' width='50' />
+                            {/* <h2>Welcome, {user.name}</h2> */}
                             <button onClick={() => signOut()}>Sign Out</button>
-                        </>
+                        </div>
                     )}
 
                     {!user && (
@@ -29,6 +29,7 @@ const Login = () => {
                     )}
                 </div>
             </login>
+
         </>
     )
 }
