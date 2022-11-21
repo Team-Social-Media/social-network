a3f9437978eadccc3aeacdd9643634575174c2c3
Hayden Cleaver <rexcleaver90@gmail.com>
2022-11-21 19:15:57 +0000

    fixing merge issue

--- components/MediaItem.js
+++ components/MediaItem.js
@@ -35,7 +35,7 @@ const style = {
   p: 4,
 };
 
-export default function MediaItem(props{ item }) {
+export default function MediaItem({ item }) {
   const [isLiked, setIsLiked] = React.useState(false);
   const [open, setOpen] = React.useState(false);
 
