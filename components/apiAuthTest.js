431ea1442162db7c8ad967d63931a4e72c0d90eb
Hayden Cleaver <rexcleaver90@gmail.com>
2022-11-21 17:49:46 +0000

    moved apiAuthTest to components; started building custom oauth provider

--- components/apiAuthTest.js
+++ components/apiAuthTest.js
@@ -2,17 +2,24 @@ import {getSession} from 'next-auth/react';
 
 // prevents API calls if no user session is detected
 
-export default async function handler(req, res) {
+export default async function handler(req, res, next) {
 
   const session = await getSession({req});
 
   console.log(session);
+  
+  try {
+
   
   if (!session) {
     return (
       res.status(401).send('Unauthorized')
     )
   }
+  
+  next();
 
-  res.status(200).json({ name: 'John Doe' })
-}
+  } catch (e) {
+    res.status(403).send('Unauthorized')
+  }
+};
