0130454c134fb47ad5f4e4cd0e322b49fe0f05a2
Isaiah Keller <isaiah.keller@gmail.com>
2022-11-22 22:22:25 +0000

    ads fix

--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,27 @@
 import styles from '../styles/Footer.module.css'
+import { useState } from 'react';
+import Image from 'next/image';
 
 const Footer = () => {
+
+  let adHeight;
+  let adWidth;
+
+  if (typeof window !== 'undefined') {
+    adWidth = Math.ceil(window.screen.availWidth / 10) || 300;
+    adHeight = Math.ceil(window.screen.availHeight / 50) || 200;
+  }
+
+  let image = () => `https://picsum.photos/450/150?random=1.jpg`;
+  console.log('footer image: ', image)
+
   return (
     <>
       <footer className={styles.footer}>
-        <h1>Ad Space</h1>
+        <div className={styles.container} >
+          <Image src={image} alt='picsum image' width='450' height='150' />
+          <h1>Your Add Here</h1>
+        </div>
       </footer>
     </>
   )
