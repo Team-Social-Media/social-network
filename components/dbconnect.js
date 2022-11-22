7e57ddc835297296a6058202f4325050b07628bf
Kris Dunning <dunning.kris@gmail.com>
2022-11-21 18:50:15 -0800

    working DB info pull @/ShowAllUsers

---
+++ components/dbconnect.js
@@ -0,0 +1,14 @@
+// import { PrismaClient } from '@prisma/client'
+
+// let prisma = new PrismaClient()
+
+// export default prisma
+
+// export async function getStaticProps() {
+//   const prisma = new PrismaClient()
+//   const users = await prisma.users.findMany()
+
+//   return {
+//     props : { users }
+//   }
+// }
