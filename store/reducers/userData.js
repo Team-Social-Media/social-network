867031fd4d7e7f646ed68c6358cd20309c6e18b7
Teresa <luvmargaritas@gmail.com>
2022-11-23 00:47:22 -0600

    adding updates

--- store/reducers/userData.js
+++ store/reducers/userData.js
@@ -3,12 +3,8 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const  USERSlice = createSlice({
   name: 'users',
-<<<<<<< HEAD
   initialState: {
   },
-=======
-  initialState: [],
->>>>>>> 3e917f731f029fd082020dd76c58599a0a1cb275
   reducers: {
     getAll: table => {
       return async() =>{
@@ -28,10 +24,6 @@ const  USERSlice = createSlice({
           const prisma = new PrismaClient()
           const user = await prisma.users.findFirst({
             where: { name: userName },
-<<<<<<< HEAD
-=======
-
->>>>>>> 3e917f731f029fd082020dd76c58599a0a1cb275
           })
           console.log('find by name from acessDB slice : ',user);
         } catch (err) {
@@ -39,22 +31,12 @@ const  USERSlice = createSlice({
         }
       }
     },
-<<<<<<< HEAD
     upsertUser: (state,data) => {
       console.log('upsert user');
       try{
         prisma.users.upsert({
         where: {
           id: [data.id],
-=======
-    upsertUser: (state, data) => {
-      console.log('upsert user');
-      try{
-        const prisma = new PrismaClient()
-        prisma.users.upsert({
-        where: {
-          email: [data.email],
->>>>>>> 3e917f731f029fd082020dd76c58599a0a1cb275
         },
         create: {
           id: [data.id],
@@ -62,11 +44,7 @@ const  USERSlice = createSlice({
           image: [data.image],
           email: [data.email],
           email_verified:'',
-<<<<<<< HEAD
           favorites:[data.favorites],
-=======
-          favorites:[...data.favorites],
->>>>>>> 3e917f731f029fd082020dd76c58599a0a1cb275
           friends:[data.friends],
           comments:[data.comments],
           posts:[data.posts],
@@ -76,25 +54,13 @@ const  USERSlice = createSlice({
       })
     }
     catch(err){
-<<<<<<< HEAD
       console.log("ERROR in upsert user");
     }
-=======
-      console.log("ERROR in upsert users", err.message);
-    }
-    },
-    favorites: (state = initialState, action) => {
-      return [...state, ...action.payload]
->>>>>>> 3e917f731f029fd082020dd76c58599a0a1cb275
     }
   }
 })
 
-<<<<<<< HEAD
 export const { getAll,getOne,upsertUser } = USERSlice.actions
-=======
-export const { getAll,getOne,upsertUser, favorites } = USERSlice.actions
->>>>>>> 3e917f731f029fd082020dd76c58599a0a1cb275
 
 export default USERSlice.reducer
 
